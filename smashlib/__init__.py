d47edaa0a53e8b5a87e7c9590a589243bda521df
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-06 13:19:46 +0000

    update readme

--- smashlib/__init__.py
+++ smashlib/__init__.py
@@ -1,41 +1,9 @@
 """ smashlib
 """
+
 def start_ipython(argv=None, **kwargs):
     from IPython.terminal.ipapp import TerminalIPythonApp as _
-    from IPython.terminal.interactiveshell import \
-         TerminalInteractiveShell as tis
-    class SmashTerminalInteractiveShell(tis):
-        def __init__(self,*args,**kargs):
-            sooper = super(SmashTerminalInteractiveShell,self)
-            sooper.__init__(*args, **kargs)
-            self._smash_last_input = ""
-        @property
-        def smash(self):
-            return getattr(self, '_smash', None)
-
-        def raw_input(self, *args, **kargs):
-            sooper = super(SmashTerminalInteractiveShell,self)
-            out = sooper.raw_input(*args, **kargs)
-            self._smash_last_input += out
-            print 'post-input'
-            return out
-
-        def run_cell(self, raw_cell, store_history=False,
-                     silent=False, shell_futures=True):
-            sooper = super(SmashTerminalInteractiveShell, self)
-            out = sooper.run_cell(
-                raw_cell, store_history=store_history,
-                silent=silent, shell_futures=shell_futures)
-            if self.smash is not None:
-                self.smash.bus.publish(
-                    C_POST_RUN_CELL,
-                    self.user_ns['In'][-1].strip())
-                self.smash.bus.publish(
-                    C_POST_RUN_INPUT,
-                    self._smash_last_input)
-                self._smash_last_input = ""
-    TerminalInteractiveShell=SmashTerminalInteractiveShell
-    from smashlib.channels import C_POST_RUN_INPUT, C_POST_RUN_CELL
+    from smashlib.overrides import TerminalInteractiveShell
     class TerminalIPythonApp(_):
         @classmethod
         def launch_instance(cls, argv=None, **kwargs):
