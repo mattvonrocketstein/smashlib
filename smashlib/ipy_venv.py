b053fac10635eed39867ad74e287e89a7eab1fad
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-10 01:09:46 +0000

    better support for cleaning user namespace when changing venvs

--- smashlib/ipy_venv.py
+++ smashlib/ipy_venv.py
@@ -4,17 +4,19 @@
     switching virtualenv's without leaving your shell.  Things are going
     to get hairy if you're using multiple python distributions.
 """
+import inspect
 import os, sys, glob
 
 from IPython.utils.traitlets import Bool
 from IPython.core.magic import Magics, magics_class, line_magic
 
+from smashlib.data import SMASH_DIR
 from smashlib.v2 import Reporter
 from smashlib.util import truncate_fpath
 from smashlib.python import opj, ope, abspath, expanduser
 from smashlib.util.events import receives_event
 from smashlib.util.venv import get_venv, is_venv, to_vbin, to_vlib, get_path
-
+from smashlib.util import get_smash
 # channel names for use with the smash bus
 C_POST_ACTIVATE = 'post_activate_venv'
 C_PRE_ACTIVATE = 'pre_activate_venv'
@@ -30,7 +32,6 @@ class VirtualEnvMagics(Magics):
 
     @line_magic
     def venv_deactivate(self, parameter_s=''):
-        self.report("venv_deactivate: "+parameter_s)
         self.vext.deactivate()
 
     @property
@@ -40,19 +41,16 @@ class VirtualEnvMagics(Magics):
 
 class VirtualEnvSupport(Reporter):
 
-    #not honored yet
-    show_venv_in_prompt = Bool(False, config=True)
-
     def deactivate(self):
-        target = os.environ.get('VIRTUAL_ENV','')
-        if not target:
-            return
-        self.publish(C_PRE_DEACTIVATE, target)
         try:
             venv = get_venv()
         except KeyError:
+            self.warning("no venv to deactivate")
             return False
         else:
+            self.report("venv_deactivate: "+venv)
+            self.publish(C_PRE_DEACTIVATE, venv)
+
             if not ope(venv):
                 self.warning('refusing to deactivate (relocated?) venv')
                 return #raise RuntimeError(err)
@@ -90,11 +88,40 @@ class VirtualEnvSupport(Reporter):
                     #else:
                         #print 'cleaning: ',entry
                     #    pass
-
             sys.path = new_path
+
+            self._clean_user_namespace()
             # TODO: clean sys.modules?
-            self.publish(C_POST_DEACTIVATE, target)
+            self.publish(C_POST_DEACTIVATE, venv)
             return True
+
+    def _clean_user_namespace(self):
+            """ clean ipython username to remove old project's code
+                TODO: document and refactor
+            """
+            self.report("cleaning user namespace")
+            names = []
+            pm = get_smash().project_manager
+            pdir = pm._current_project and pm.project_map[pm._current_project]
+            namespace = get_smash().shell.user_ns
+            for name,obj in namespace.items():
+                try:
+                    fname = inspect.getfile(obj)
+                except TypeError:
+                    # happens for anything that's not a module, class
+                    # method, function, traceback, frame or code object
+                    fname = None
+                if fname:
+                    test = fname.startswith(venv)
+                    if pdir is not None:
+                        test = test or fname.startswith(pdir)
+                    if test and \
+                        not name.startswith('_') and \
+                        not name.startswith(SMASH_DIR):
+                        names.append(name)
+            for name in names:
+                del self.smash.shell.user_ns[name]
+            self.report("wiped from namespace: {0}".format(names))
 
     def activate(self, path):
         self.deactivate()
